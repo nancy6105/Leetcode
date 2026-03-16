7f8aae4a4cf550db63cf57f375eb2695db38f534
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-03-16 16:53:32 +0530

    0120-triangle N/A (0%) N/A (0%)

--- LeetCode/Medium/0120-triangle/0120-triangle.java
+++ LeetCode/Medium/0120-triangle/0120-triangle.java
@@ -14,16 +14,4 @@ class Solution {
         }
         return dp[0][0]; 
     }
-    int sol(List<List<Integer>> triangle,int i,int j,int dp[][]){
-        if(i == triangle.size()-1){
-            return triangle.get(triangle.size()-1).get(j);
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        int down = triangle.get(i).get(j) + sol(triangle,i+1,j,dp);
-        int diagonal = triangle.get(i).get(j) + sol(triangle,i+1,j+1,dp);
-
-        return dp[i][j] = Math.min(down,diagonal);
-    }
 }
