1f2b7d1dbef513e8a35a053dd354defea34467ba
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-03-17 16:13:59 +0530

    1463-cherry-pickup-ii N/A (0%) N/A (0%)

--- LeetCode/Hard/1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ LeetCode/Hard/1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -3,47 +3,44 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
         int dp[][][] = new int[n][m][m];
-        for(int i = 0;i<n;i++){
-            for(int j=0;j<m;j++){
-                Arrays.fill(dp[i][j],-1);
-            }
-        }
-        return sol(0,0,m-1,grid,dp);
-    }
-    int sol(int i,int j1,int j2,int[][] a,int dp[][][]){
-        int n = a.length;
-        int m = a[0].length;
-
-        if(j1 < 0 || j1 >= m || j2 < 0 || j2 >= m){
-            return (int)-1e9;
-        }
-        if(i == n-1){
-            if(j1 == j2){
-                return a[i][j1];
-            }
-            else{
-                return a[i][j1] + a[i][j2];
-            }
-        }
-        if(dp[i][j1][j2] != -1){
-            return dp[i][j1][j2];
-        }
-        int maxi = Integer.MIN_VALUE;
-        for(int d1 = -1; d1 <= 1; d1++){
-            for(int d2 = -1; d2 <= 1; d2++){
-                int value;
-
+        for(int j1 = 0;j1<m;j1++){
+            for(int j2 = 0;j2 < m; j2++){
                 if(j1 == j2){
-                    value = a[i][j1];
+                    dp[n-1][j1][j2] = grid[n-1][j1];
                 }
                 else{
-                    value = a[i][j1] + a[i][j2];
+                    dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
                 }
-
-                value += sol(i+1,j1+d1,j2+d2,a,dp);
-                maxi = Math.max(maxi,value);
             }
         }
-        return dp[i][j1][j2] = maxi;
+
+        for(int i = n-2;i>=0;i--){
+            for(int j1 = 0;j1<m;j1++){
+                for(int j2 = 0;j2<m;j2++){
+                    int maxi = Integer.MIN_VALUE;
+                    for(int d1 = -1; d1 <= 1; d1++){
+                        for(int d2 = -1; d2 <= 1; d2++){
+                            int value;
+
+                            if(j1 == j2){
+                                value = grid[i][j1];
+                            }
+                            else{
+                                value = grid[i][j1] + grid[i][j2];
+                            }
+                            if(j1+d1 >= 0 && j1+d1 < m && j2+d2 >= 0 && j2+d2 < m){
+                                value += dp[i+1][j1+d1][j2+d2];
+                            }
+                            else{
+                                value += (int)-1e9;
+                            }
+                            maxi = Math.max(maxi,value);
+                        }
+                    }
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+        return dp[0][0][m-1];
     }
 }
