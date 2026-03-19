517b418bb434e092059762994800222d91069003
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-03-19 12:01:20 +0530

    0300-longest-increasing-subsequence N/A (0%) N/A (0%)

--- LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -2,23 +2,17 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         
         int n = nums.length;
-        int dp[][] = new int[n][n+1];
-        for(int i = 0;i<n;i++){
-            Arrays.fill(dp[i],-1);
+        int dp[][] = new int[n+1][n+1];
+        for(int i = n-1;i>=0;i--){
+            for(int j = i-1;j>=-1;j--){
+                int notpick = dp[i+1][j+1];
+                int pick = 0;
+                if(j == -1 || nums[i] > nums[j]){
+                    pick = 1 + dp[i+1][i+1];
+                }
+                dp[i][j+1] = Math.max(pick,notpick); 
+            }
         }
-        return sol(0,-1,nums,dp);
-    }
-    int sol(int idx,int prev,int nums[],int dp[][]){
-        if(idx == nums.length)return 0;
-
-        if(dp[idx][prev+1] != -1){
-            return dp[idx][prev+1];
-        }
-        int notpick = sol(idx+1,prev,nums,dp);
-        int pick = 0;
-        if(prev == -1 || nums[idx] > nums[prev]){
-            pick = 1+sol(idx+1,idx,nums,dp);
-        } 
-        return dp[idx][prev+1] = Math.max(pick,notpick);
+        return dp[0][0];
     }
 }
