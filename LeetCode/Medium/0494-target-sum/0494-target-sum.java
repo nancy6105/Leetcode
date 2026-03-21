12074b828caa5f21f76c49c609e5ec3b41735f4d
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-03-21 13:10:43 +0530

    0494-target-sum N/A (0%) N/A (0%)

--- LeetCode/Medium/0494-target-sum/0494-target-sum.java
+++ LeetCode/Medium/0494-target-sum/0494-target-sum.java
@@ -1,40 +1,41 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
-        int s = 0;
-        for(int i : nums){
-            s+=i;
+        int total = 0;
+        for (int i : nums) {
+            total += i;
         }
-        int k = (target + s)/2;
-        
-        if(Math.abs(target) > s)return 0;
 
-        if((s+target)%2 != 0)return 0;
-        
-        int dp[][] = new int[n][k+1];
+        int k = (total + target) / 2;
+        if (Math.abs(target) > total)
+            return 0;
 
-        for(int i = 0;i<n;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return sol(0,0,k,nums,dp);
-    }
-    int sol(int idx,int sum,int k,int a[],int dp[][]){
-        if(idx == a.length){
-            if(sum == k)return 1;
-            else{
-                return 0;
+        if ((total + target) % 2 != 0)
+            return 0;
+
+        int dp[][] = new int[n][k + 1];
+
+        if (nums[0] == 0) {
+            dp[0][0] = 2;
+        } else {
+            dp[0][0] = 1;
+            if (nums[0] <= k) {
+                dp[0][nums[0]] = 1;
             }
         }
 
-        if(dp[idx][sum] != -1){
-            return dp[idx][sum];
-        }
-        int notpick = sol(idx+1,sum,k,a,dp);
-        int pick = 0;
-        if(sum + a[idx] <= k){
-            pick = sol(idx+1,sum+a[idx],k,a,dp);
+        for (int i = 1; i < n; i++) {
+            for (int t = 0; t <= k; t++) {
+                int notpick = dp[i - 1][t];
+                int pick = 0;
+                if (nums[i] <= t) {
+                    pick = dp[i-1][t - nums[i]];
+                }
+
+                dp[i][t] = pick + notpick;
+            }
         }
 
-        return dp[idx][sum] = pick+notpick;
+        return dp[n - 1][k];
     }
 }
