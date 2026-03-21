8fb3b88c3ed2e0d35fe5347e92a778f5e4ed7ec5
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-03-21 13:29:37 +0530

    0494-target-sum N/A (0%) N/A (0%)

--- LeetCode/Medium/0494-target-sum/0494-target-sum.java
+++ LeetCode/Medium/0494-target-sum/0494-target-sum.java
@@ -13,29 +13,31 @@ class Solution {
         if ((total + target) % 2 != 0)
             return 0;
 
-        int dp[][] = new int[n][k + 1];
-
+        //int dp[][] = new int[n][k + 1];
+        int prev[] = new int[k+1];
         if (nums[0] == 0) {
-            dp[0][0] = 2;
+            prev[0] = 2;
         } else {
-            dp[0][0] = 1;
+            prev[0] = 1;
             if (nums[0] <= k) {
-                dp[0][nums[0]] = 1;
+                prev[nums[0]] = 1;
             }
         }
 
         for (int i = 1; i < n; i++) {
+            int cur[] = new int[k+1];
             for (int t = 0; t <= k; t++) {
-                int notpick = dp[i - 1][t];
+                int notpick = prev[t];
                 int pick = 0;
                 if (nums[i] <= t) {
-                    pick = dp[i-1][t - nums[i]];
+                    pick = prev[t - nums[i]];
                 }
 
-                dp[i][t] = pick + notpick;
+                cur[t] = pick + notpick;
             }
+            prev = cur;
         }
 
-        return dp[n - 1][k];
+        return prev[k];
     }
 }
