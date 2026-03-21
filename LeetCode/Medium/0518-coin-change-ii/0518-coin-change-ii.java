3ed8bdf284d26517c59039891734d3c37b76f287
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-03-21 22:56:00 +0530

    0518-coin-change-ii N/A (0%) N/A (0%)

--- LeetCode/Medium/0518-coin-change-ii/0518-coin-change-ii.java
+++ LeetCode/Medium/0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,47 +1,30 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int dp[][] = new int[n][amount+1];
-        for(int i = 0;i<n;i++){
-            Arrays.fill(dp[i],-1);
-        }
+        
+        int prev[] = new int[amount+1];
+
         for(int t = 0;t<=amount;t++){
             if(t%coins[0] == 0){
-                dp[0][t] = 1;
+                prev[t] = 1;
             }
             else{
-                dp[0][t] = 0;
+                prev[t] = 0;
             }
         }
 
         for(int i = 1;i<n;i++){
+            int curr[] = new int[amount+1];
             for(int t = 0;t<=amount;t++){
-                int notpick = dp[i-1][t];
+                int notpick = prev[t];
                 int pick = 0;
                 if(coins[i] <= t){
-                    pick = dp[i][t-coins[i]];
+                    pick = curr[t-coins[i]];
                 }
-                dp[i][t] = pick+notpick;
+                curr[t] = pick+notpick;
             }
+            prev = curr;
         }
-        return dp[n-1][amount];
-    }
-    int sol(int i,int k,int a[],int dp[][]){
-        if(i == 0){
-            if(k%a[0] == 0){
-                return 1;
-            }
-            else{
-                return 0;
-            }
-        }
-        if(dp[i][k] != -1)return dp[i][k];
-
-        int notpick = sol(i-1,k,a,dp);
-        int pick = 0;
-        if(a[i] <= k){
-            pick = sol(i,k-a[i],a,dp);
-        }
-        return dp[i][k] = pick + notpick;
+        return prev[amount];
     }
 }
