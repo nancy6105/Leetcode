5dfc8a6009c8e373fe8735297411d44d5e5b02ca
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-04-25 23:51:19 +0530

    0053-maximum-subarray 1 ms (99.97%) 77.2 MB (50.41%)

---
+++ LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int max = nums[0];
+        int curr = nums[0];
+
+        for(int i = 1; i < n; i++){
+            curr =  Math.max(nums[i], curr + nums[i]);
+            max = Math.max(curr,max);
+        }
+
+        return max;
+    }
+}
