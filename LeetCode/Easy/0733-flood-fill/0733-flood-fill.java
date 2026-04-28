ceed5a07437c04d546fbf7b191c53322402b3fd4
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-04-28 18:03:01 +0530

    0733-flood-fill N/A (0%) N/A (0%)

--- LeetCode/Easy/0733-flood-fill/0733-flood-fill.java
+++ LeetCode/Easy/0733-flood-fill/0733-flood-fill.java
@@ -1,42 +1,27 @@
 class Solution {
-    class Pair{
-        int first;
-        int second;
-
-        public Pair(int first,int second){
-            this.first = first;
-            this.second = second;
-        }
-    }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int n = image.length;
         int m = image[0].length;
         int org = image[sr][sc];
 
-        if(image[sr][sc] == color)return image;
+        if(org == color) return image;
 
-        Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(sr,sc));
-
-        image[sr][sc] = color;
-        int [] dr = {-1,1,0,0};
-        int [] dc = {0,0,-1,1};
-        while(!q.isEmpty()){
-            Pair p = q.poll();
-            int row = p.first;
-            int col = p.second;
-
-            for(int k = 0;k<4;k++){
-                int nr = row + dr[k];
-                int nc = col + dc[k];
-
-                if(nr >= 0 && nc >= 0 && nr < n && nc < m && image[nr][nc] == org){
-                    image[nr][nc] = color;
-                    q.offer(new Pair(nr,nc));
-                }
-            } 
-        }
+        dfs(image,sr,sc,org,color);
         return image;
+    }
+    void dfs(int [][]image,int i,int j,int org,int color){
+        int n = image.length;
+        int m = image[0].length;
+        
+        if(i < 0 || j < 0 || i >= n || j >= m || image[i][j] != org){
+            return;
+        }
+
+        image[i][j] = color;
+        dfs(image,i-1,j,org,color);
+        dfs(image,i+1,j,org,color);
+        dfs(image,i,j-1,org,color);
+        dfs(image,i,j+1,org,color);
 
     }
 }
