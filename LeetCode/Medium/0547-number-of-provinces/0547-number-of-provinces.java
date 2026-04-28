f3bcf0089704d6031fd519cf2ee2e81c906eb6fe
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-04-28 13:09:10 +0530

    0547-number-of-provinces 5 ms (10.2%) 48.9 MB (20.66%)

--- LeetCode/Medium/0547-number-of-provinces/0547-number-of-provinces.java
+++ LeetCode/Medium/0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,21 +1,40 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         int v = isConnected.length;
+        for(int i = 0;i<v;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i = 0;i<v;i++){
+            for(int j = 0;j<v;j++){
+                if(isConnected[i][j] == 1 && i != j){
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
+                }
+            }
+        }
         boolean visited[] = new boolean[v];
         int cnt = 0;
         for(int i = 0;i<v;i++){
             if(!visited[i]){
+                bfs(i,adj,visited);
                 cnt++;
-                dfs(i,visited,isConnected);
             }
         }
         return cnt;
     }
-    void dfs(int node,boolean visited[],int adj[][]){
-        visited[node] = true;
-        for(int i = 0;i<adj.length;i++){
-            if(adj[node][i] == 1 && !visited[i]){
-                dfs(i,visited,adj);
+    void bfs(int n, ArrayList<ArrayList<Integer>>adj, boolean visited[]){
+        visited[n] = true;
+        Queue<Integer>q = new LinkedList<>();
+        q.add(n);
+        
+        while(!q.isEmpty()){
+            int node = q.poll();
+            for(int nei : adj.get(node)){
+                if(!visited[nei]){
+                    visited[nei] = true;
+                    q.add(nei);
+                }
             }
         }
     }
