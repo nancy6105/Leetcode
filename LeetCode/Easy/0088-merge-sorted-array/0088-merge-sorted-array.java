c8c268ab54342169a94e31e4bc6caee1b5267de8
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-18 01:01:05 +0530

    0088-merge-sorted-array 0 ms (100%) 43.9 MB (42.63%)

--- LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.java
+++ LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,29 +1,21 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = 0;
-        int j = 0;
+        int i = m-1;
+        int j = n-1;
 
-        ArrayList<Integer> arr = new ArrayList<>();
-        while(i < m && j < n){
-            if(nums1[i] <= nums2[j]){
-                arr.add(nums1[i++]);
+        int k = m+n-1;
+
+        while(i >= 0 && j >= 0){
+            if(nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--];
             }
-
             else{
-                arr.add(nums2[j++]);
+                nums1[k--] = nums2[j--];
             }
         }
 
-        while(i < m){
-            arr.add(nums1[i++]);
-        }
-
-        while(j < n){
-            arr.add(nums2[j++]);
-        }
-
-        for(int k = 0; k < m+n; k++){
-            nums1[k] = arr.get(k);
+        while(j >= 0){
+            nums1[k--] = nums2[j--];
         }
     }
 }
