46dc30bb5a78b2b560d5cdc6336acb3debc2a07c
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-19 01:25:03 +0530

    0080-remove-duplicates-from-sorted-array-ii 0 ms (100%) 49 MB (14.52%)

--- LeetCode/Medium/0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ LeetCode/Medium/0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -5,24 +5,14 @@ class Solution {
             return nums.length;
         }
 
-        int cnt = 1;
-        int k = 1;
+        int k = 2;
 
-        for(int i = 1;i<n;i++){
-            if(nums[i] == nums[i-1]){
-                cnt++;
-            }
-
-            else{
-                cnt = 1; 
-            }
-
-            if(cnt <= 2){
+        for(int i = 2; i < n; i++){
+            if(nums[i] != nums[k-2]){
                 nums[k] = nums[i];
                 k++;
             }
         }
-
         return k;
     }
 }
