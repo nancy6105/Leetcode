1d06f5452f538d3f98871819ff1322df183d4d19
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-20 02:48:51 +0530

    0055-jump-game 2 ms (87.94%) 47.8 MB (60.67%)

--- LeetCode/Medium/0055-jump-game/0055-jump-game.java
+++ LeetCode/Medium/0055-jump-game/0055-jump-game.java
@@ -1,27 +1,17 @@
 class Solution {
     public boolean canJump(int[] nums) {
+        int farthest = 0;
         int n = nums.length;
-        int dp[] = new int[n];
+        for(int i = 0;i<n;i++){
+            if(i > farthest){
+                return false;
+            }
 
-        Arrays.fill(dp,-1);
-        return sol(nums,0,dp);
-    }
-    private boolean sol(int[] arr,int idx,int dp[]){
-        if(idx >= arr.length-1){
-            return true;
-        }
-
-        if(dp[idx] != -1){
-            return dp[idx] == 1;
-        }
-        for(int jump = 1;jump <= arr[idx];jump++){
-            if(sol(arr,idx+jump,dp)){
-                dp[idx] = 1;
+            farthest = Math.max(farthest,i + nums[i]);
+            if(farthest >= n-1){
                 return true;
             }
         }
-
-        dp[idx] = 0;
-        return false;
+        return true;
     }
 }
