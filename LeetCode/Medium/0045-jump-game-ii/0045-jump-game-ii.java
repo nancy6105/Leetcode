3f5337d9798da78aed289b1450734363593a4a52
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-21 01:12:08 +0530

    0045-jump-game-ii 1 ms (99.58%) 47.4 MB (20.44%)

--- LeetCode/Medium/0045-jump-game-ii/0045-jump-game-ii.java
+++ LeetCode/Medium/0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,28 +1,18 @@
 class Solution {
     public int jump(int[] nums) {
-        int n = nums.length;
-        int dp[] = new int[n];
-        Arrays.fill(dp,-1);
+        int jumps = 0;
+        int current = 0;
 
-        return sol(nums,0,dp);
-    }
-    int sol(int arr[], int idx, int dp[]){
-        if(idx >= arr.length-1){
-            return 0;
-        }
+        int farthest = 0;
 
-        if(dp[idx] != -1){
-            return dp[idx];
-        }
-        int min = Integer.MAX_VALUE;
-
-        for(int i = 1;i <= arr[idx];i++){
-            int ans = sol(arr, i + idx,dp);
-            if(ans != Integer.MAX_VALUE){
-                min = Math.min(1 + ans,min);
+        for(int i = 0; i<nums.length-1; i++){
+            farthest = Math.max(farthest, i + nums[i]);
+            if(i == current){
+                jumps++;
+                current = farthest;
             }
         }
 
-        return dp[idx] = min;
+        return jumps;
     }
 }
