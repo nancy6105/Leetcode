319f0c39b32717f8213c32cb4f8977ecb1f34341
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-23 01:41:06 +0530

    0120-triangle 5 ms (37.58%) 45.4 MB (60.49%)

--- LeetCode/Medium/0120-triangle/0120-triangle.java
+++ LeetCode/Medium/0120-triangle/0120-triangle.java
@@ -1,17 +1,21 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int m = triangle.size();
-        int dp[][] = new int[m][m];
-        for(int j = 0;j<m;j++){
-            dp[m-1][j] = triangle.get(m-1).get(j);
+        int n = triangle.size();
+        int dp[][] = new int[n][n];
+        for(int j = 0; j < n; j++){
+            dp[n-1][j] = triangle.get(n-1).get(j);
         }
-        for(int i = m-2;i >= 0;i--){
-            for(int j = i;j >= 0;j--){
+
+        for(int i = n-2; i >= 0; i--){
+            for(int j = 0; j <= i; j++){
+
                 int down = triangle.get(i).get(j) + dp[i+1][j];
                 int diagonal = triangle.get(i).get(j) + dp[i+1][j+1];
+
                 dp[i][j] = Math.min(down,diagonal);
             }
         }
-        return dp[0][0]; 
+
+        return dp[0][0];
     }
 }
