2586b36323a80bc230d2c4b6baa2e6f50fd49f1e
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-23 23:51:58 +0530

    0063-unique-paths-ii 1 ms (17.04%) 43.7 MB (26.25%)

--- LeetCode/Medium/0063-unique-paths-ii/0063-unique-paths-ii.java
+++ LeetCode/Medium/0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -4,16 +4,35 @@ class Solution {
         int m = obstacleGrid[0].length;
 
         int dp[][] = new int[n][m];
-        for(int rows[] : dp){
-            Arrays.fill(rows,-1);
+        for(int i = n-1; i >= 0; i--){
+            for(int j = m-1;j >= 0; j--){
+                if(obstacleGrid[i][j] == 1){
+                    dp[i][j] = 0;
+                }
+                else if(i == n-1 && j == m-1){
+                    dp[i][j] = 1;
+                }
+
+                else{
+                    int down = 0;
+                    int right = 0;
+                    if(i < n-1){
+                        down = dp[i+1][j];
+                    }
+                    if(j < m-1){
+                        right = dp[i][j+1]; 
+                    }
+                    dp[i][j] = down+right;
+                }
+            }
         }
-        return sol(0,0,obstacleGrid,dp);
+        return dp[0][0];
     }
     int sol(int i,int j,int[][] arr,int dp[][]){
         int n = arr.length;
         int m = arr[0].length;
         if(i < n && j < m && arr[i][j] == 1){
-            return 0;
+            return 0;   
         }
         if(i == n-1 && j == m-1){
             return 1;
