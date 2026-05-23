ed3de4252661b4e87c8d39d0425147e6b0830688
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-23 13:39:19 +0530

    0064-minimum-path-sum 7 ms (10.11%) 50 MB (83.11%)

--- LeetCode/Medium/0064-minimum-path-sum/0064-minimum-path-sum.java
+++ LeetCode/Medium/0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -5,11 +5,29 @@ class Solution {
         
         int dp[][] = new int[n][m];
 
-        for(int rows[] : dp){
-            Arrays.fill(rows,-1);
-        }
+        for(int i = n-1; i >= 0; i--){
+            for(int j = m-1; j >= 0; j--){
 
-        return sol(0,0,grid,dp);
+                if(i == n-1 && j == m-1){
+                    dp[i][j] = grid[i][j];
+                }
+
+                else{
+                    int down = (int)1e9;
+                    int right = (int)1e9;
+
+                    if(i+1 < n){
+                        down = grid[i][j] + dp[i+1][j];
+                    }
+                    if(j+1 < m){
+                        right = grid[i][j] + dp[i][j+1];
+                    }
+
+                    dp[i][j] = Math.min(right,down);
+                }
+            }
+        }
+        return dp[0][0];
     }
     public int sol(int i, int j, int[][]arr, int dp[][]){
         if(i == arr.length-1 && j == arr[0].length-1){
@@ -23,7 +41,7 @@ class Solution {
         if(dp[i][j] != -1){
             return dp[i][j];
         }
-        
+
         int down = arr[i][j] + sol(i+1,j,arr,dp);
         int diagonal = arr[i][j] + sol(i,j+1,arr,dp);
 
