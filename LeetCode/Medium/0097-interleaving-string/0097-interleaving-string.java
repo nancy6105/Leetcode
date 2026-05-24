47a68ea4ae451a72d4e2431665e8cea4e0840fcb
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-24 14:24:12 +0530

    0097-interleaving-string 2 ms (76.46%) 46.1 MB (6.61%)

--- LeetCode/Medium/0097-interleaving-string/0097-interleaving-string.java
+++ LeetCode/Medium/0097-interleaving-string/0097-interleaving-string.java
@@ -2,20 +2,17 @@ class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         if(s1.length() + s2.length() != s3.length()) return false;
 
-        int dp[][][] = new int[101][101][201];
-        for(int rows[][] : dp){
-            for(int row[] : rows){
-                Arrays.fill(row, -1);
-            }
+        int dp[][] = new int[101][101];
+        for(int rows[] : dp){
+            Arrays.fill(rows,-1);
         }
 
         int i = 0;
         int j = 0;
-        int k = 0;
-
-        return sol(i,j,k,s1,s2,s3,dp);
+        
+        return sol(i,j,s1,s2,s3,dp);
     }
-    boolean sol(int i, int j, int k, String s1, String s2, String s3,int dp[][][]){
+    boolean sol(int i, int j, String s1, String s2, String s3,int dp[][]){
         int n = s1.length();
         int m = s2.length();
         int N = n+m;
@@ -23,22 +20,22 @@ class Solution {
             return true;
         }
 
-        if(dp[i][j][k] != -1){
-            return dp[i][j][k] == 1;
+        if(dp[i][j] != -1){
+            return dp[i][j] == 1;
         }
 
         boolean res = false;
-        if(i < n && s1.charAt(i) == s3.charAt(k)){
-            res = sol(i+1,j,k+1,s1,s2,s3,dp);
+        if(i < n && s1.charAt(i) == s3.charAt(i+j)){
+            res = sol(i+1,j,s1,s2,s3,dp);
         }
         if(res){
             return res;
         }
-        if(j < m && s2.charAt(j) == s3.charAt(k)){
-            res = sol(i,j+1,k+1,s1,s2,s3,dp);
+        if(j < m && s2.charAt(j) == s3.charAt(i+j)){
+            res = sol(i,j+1,s1,s2,s3,dp);
         }
 
-        dp[i][j][k] = (res) ? 1 : 0;
+        dp[i][j] = (res) ? 1 : 0;
         return res;
-    }
+    }   
 }
