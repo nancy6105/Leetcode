475688847ee5e449f07f1b2fc80b70f3f5aeb2e5
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-25 16:11:35 +0530

    0123-best-time-to-buy-and-sell-stock-iii 68 ms (26.25%) 88.1 MB (40.71%)

--- LeetCode/Hard/0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ LeetCode/Hard/0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -26,29 +26,4 @@ class Solution {
         return after[1][2];
     }
 
-    int sol(int idx, int buy, int arr[], int cap, int dp[][][]) {
-        int n = arr.length;
-        if (cap == 0) {
-            return 0;
-        }
-
-        if (idx == n) {
-            return 0;
-        }
-        if (dp[idx][buy][cap] != -1) {
-            return dp[idx][buy][cap];
-        }
-
-        int profit = 0;
-        if (buy == 1) {
-            int take = -arr[idx] + sol(idx + 1, 0, arr, cap, dp);
-            int nottake = 0 + sol(idx + 1, 1, arr, cap, dp);
-            profit = Math.max(take, nottake);
-        } else {
-            int take = arr[idx] + sol(idx + 1, 1, arr, cap - 1, dp);
-            int nottake = 0 + sol(idx + 1, 0, arr, cap, dp);
-            profit = Math.max(take, nottake);
-        }
-        return dp[idx][buy][cap] = profit;
-    }
 }
