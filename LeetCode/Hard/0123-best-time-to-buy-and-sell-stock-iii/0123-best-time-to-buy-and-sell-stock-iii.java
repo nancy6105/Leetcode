d244d12a9a6b67e284668af8a8570c0ae76bcde2
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-25 16:11:12 +0530

    0123-best-time-to-buy-and-sell-stock-iii 59 ms (31.33%) 88 MB (40.71%)

--- LeetCode/Hard/0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ LeetCode/Hard/0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,39 +1,29 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int dp[][][] = new int[n + 1][2][3];
-
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<=1;j++){
-                dp[i][j][0] = 0;
-            }
-        }
-
-        for(int buy = 0;buy <= 1;buy++){
-            for(int cap = 0;cap <= 2;cap++){
-                dp[n][buy][cap] = 0;
-            }
-        }
+        int after[][] = new int[2][3];
 
         for (int idx = n - 1; idx >= 0; idx--) {
+            int curr[][] = new int[2][3];
             for (int buy = 0; buy <= 1; buy++) {
                 for (int cap = 1; cap <= 2; cap++) {
                     int profit = 0;
 
                     if (buy == 1) {
-                        int take = -prices[idx] + dp[idx + 1][0][cap];
-                        int nottake = 0 + dp[idx + 1][1][cap];
+                        int take = -prices[idx] + after[0][cap];
+                        int nottake = 0 + after[1][cap];
                         profit = Math.max(take, nottake);
                     } else {
-                        int take = prices[idx] + dp[idx + 1][1][cap-1];
-                        int nottake = 0 + dp[idx + 1][0][cap];
+                        int take = prices[idx] + after[1][cap-1];
+                        int nottake = 0 + after[0][cap];
                         profit = Math.max(take, nottake);
                     }
-                    dp[idx][buy][cap] = profit;
+                    curr[buy][cap] = profit;
                 }
             }
+            after = curr;
         }
-        return dp[0][1][2];
+        return after[1][2];
     }
 
     int sol(int idx, int buy, int arr[], int cap, int dp[][][]) {
