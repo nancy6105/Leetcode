7d5c7d5cbd7f0b714e5d549dfc0844e0eedc15b3
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-26 01:51:59 +0530

    0322-coin-change 16 ms (64.53%) 46.9 MB (32.26%)

--- LeetCode/Medium/0322-coin-change/0322-coin-change.java
+++ LeetCode/Medium/0322-coin-change/0322-coin-change.java
@@ -1,29 +1,31 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int dp[][] = new int[n][amount+1];
+        int prev[] = new int[amount+1];
         for(int amt = 0; amt <= amount; amt++){
             if(amt%coins[0] == 0){
-                dp[0][amt] = amt/coins[0];
+                prev[amt] = amt/coins[0];
             }
             else{
-                dp[0][amt] = (int)1e9;
+                prev[amt] = (int)1e9;
             }
         }
 
         for(int i = 1; i<n; i++){
+            int curr[] = new int[amount+1];
             for(int amt = 0; amt <= amount; amt++){
-                int notpick = dp[i-1][amt];
+                int notpick = prev[amt];
                 int pick = (int)1e9;
                 if(coins[i] <= amt){
-                    pick = 1 + dp[i][amt-coins[i]];
+                    pick = 1 + curr[amt-coins[i]];
                 }
 
-                dp[i][amt] = Math.min(pick,notpick);
+                curr[amt] = Math.min(pick,notpick);
             }
+            prev = curr;
         }
 
-        int ans = dp[n-1][amount];
+        int ans = prev[amount];
         return ans >= (int)1e9 ? -1 : ans;
     }
 }
