d139b816780c0984d2912821321aed2ebfcc11d2
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-25 14:59:17 +0530

    0122-best-time-to-buy-and-sell-stock-ii 4 ms (9.25%) 50.9 MB (5.09%)

--- LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,14 +1,29 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int totalProfit = 0;
-
-        for(int i = 1;i<prices.length;i++){
-            if(prices[i-1] < prices[i]){
-                int currentProfit = prices[i]-prices[i-1];
-                totalProfit += currentProfit;
-            }
+        int n = prices.length;
+        int dp[][] = new int[n][2];//0 -> false, 1 -> true
+        for(int row[] : dp){
+            Arrays.fill(row,-1);
+        }
+        return sol(0, 1, prices, dp);
+    }
+    int sol(int idx, int buy, int[] arr,int dp[][]){
+        if(idx == arr.length){
+            return 0;
         }
 
-        return totalProfit;
+        if(dp[idx][buy] != -1){
+            return dp[idx][buy];
+        }
+
+        int profit = 0;
+        
+        if(buy == 1){
+            profit = Math.max(profit - arr[idx] + sol(idx+1, 0, arr,dp), 0 + sol(idx+1, 1, arr,dp));    
+        }
+        else{
+            profit = Math.max(profit + arr[idx] + sol(idx+1, 1, arr, dp), 0 + sol(idx+1, 0, arr, dp));    
+        }
+        return dp[idx][buy] = profit;
     }
 }
