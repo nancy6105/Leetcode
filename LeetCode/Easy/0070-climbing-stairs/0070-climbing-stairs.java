97626db25d8e7ed810f75cd2e204fb56630cc35e
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-25 20:59:03 +0530

    0070-climbing-stairs 0 ms (100%) 41.4 MB (99.38%)

---
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+        return sol(n,dp);
+    }
+    int sol(int idx,int dp[]){
+        if(idx == 0 || idx == 1){
+            return 1;
+        }
+        if(dp[idx] != -1){
+            return dp[idx];
+        }
+        return dp[idx] = sol(idx-1,dp) + sol(idx-2,dp);
+    }   
+}
