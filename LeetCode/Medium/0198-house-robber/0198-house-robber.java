c106ea6720985a3dbf749a627670e9dad21786dd
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-25 21:36:31 +0530

    0198-house-robber 0 ms (100%) 42.8 MB (54.74%)

--- LeetCode/Medium/0198-house-robber/0198-house-robber.java
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.java
@@ -2,25 +2,15 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         int dp[] = new int[n];
-        Arrays.fill(dp,-1);
-        return sol(n-1,nums,dp);
-    }
-    int sol(int idx, int nums[],int dp[]){
-        if(idx < 0){
-            return 0;
-        }
-        if(idx == 0){
-            return nums[idx];
-        }
-
-        if(dp[idx] != -1){
-            return dp[idx];
-        }
-
-        int pick = nums[idx] + sol(idx-2,nums,dp);
-        int notpick = 0 + sol(idx-1,nums,dp );
-
-        return dp[idx] = Math.max(pick,notpick);
-
+        dp[0] = nums[0];
+        for(int i = 1; i < n; i++){
+            int pick = nums[i];
+            if(i > 1){
+                pick += dp[i-2];
+            }
+            int notpick = dp[i-1];
+            dp[i] = Math.max(pick,notpick);
+        } 
+        return dp[n-1];
     }
 }
