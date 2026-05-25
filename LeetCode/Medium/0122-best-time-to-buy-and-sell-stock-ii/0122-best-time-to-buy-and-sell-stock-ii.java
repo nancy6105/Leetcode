d9965a09004941fdb5cc8b85f5c9bced3a3b29ef
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-25 15:19:45 +0530

    0122-best-time-to-buy-and-sell-stock-ii 0 ms (100%) 46.5 MB (46.34%)

--- LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ LeetCode/Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,26 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        int profit = 0;
+
         int n = prices.length;
-        int ahead[] = new int[2];
-        ahead[0] = 0;
-        ahead[1] = 0;
-
-        for(int i = n-1;i >= 0; i--){
-            int curr[] = new int[2];
-
-            for(int j = 0;j <= 1;j++){
-                int profit = 0;
-
-                if(j == 1){
-                    profit = Math.max(profit - prices[i] + ahead[0], 0 + ahead[1]);
-                }
-                else{
-                    profit = Math.max(profit + prices[i] + ahead[1], 0 + ahead[0]);
-                }
-                curr[j] = profit;
+        for(int i = 1;i<n;i++){
+            if(prices[i] > prices[i-1]){
+                profit += prices[i] - prices[i-1];
             }
-            ahead = curr;
         }
-        return ahead[1];
+        return profit;
     }
 }
