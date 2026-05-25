658ed20d1614ff9be4f3a51d019ef1b6bb415d33
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-25 17:05:12 +0530

    0188-best-time-to-buy-and-sell-stock-iv 2 ms (70.04%) 45.2 MB (43.73%)

--- LeetCode/Hard/0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ LeetCode/Hard/0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,25 +1,27 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
-        int dp[][][] = new int[n+1][2][k+1];
+        int after[][] = new int[2][k+1];
         for(int idx = n-1;idx >= 0;idx--){
+            int curr[][] = new int[2][k+1];
             for(int buy = 0;buy <= 1;buy++){
                 for(int trans = 1; trans < k+1; trans++){
                     int profit = 0;
                     if(buy == 1){
-                        int take = -prices[idx] + dp[idx+1][0][trans];
-                        int nottake = 0 + dp[idx+1][1][trans];
+                        int take = -prices[idx] + after[0][trans];
+                        int nottake = 0 + after[1][trans];
                         profit = Math.max(take,nottake);
                     }
                     else{
-                        int take = prices[idx] + dp[idx+1][1][trans-1];
-                        int nottake = 0 + dp[idx+1][0][trans];
+                        int take = prices[idx] + after[1][trans-1];
+                        int nottake = 0 + after[0][trans];
                         profit = Math.max(take,nottake);
                     }
-                    dp[idx][buy][trans] = profit;
+                    curr[buy][trans] = profit;
                 }
             }
+            after = curr;
         }
-        return dp[0][1][k];
+        return after[1][k];
     }
 }
