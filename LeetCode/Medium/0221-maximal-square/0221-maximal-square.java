66bb1c0400d3851984efc68e09dbeacb297a1084
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-25 19:36:02 +0530

    0221-maximal-square 6 ms (98.19%) 70.7 MB (86.92%)

--- LeetCode/Medium/0221-maximal-square/0221-maximal-square.java
+++ LeetCode/Medium/0221-maximal-square/0221-maximal-square.java
@@ -2,23 +2,26 @@ class Solution {
     public int maximalSquare(char[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        int dp[][] = new int[n+1][m+1];
-        
-        int max = 0;
-        
-        for(int i = n-1; i >= 0; i--){
-            for(int j = m-1; j >= 0; j--){
-                if(matrix[i][j] == '1'){
-                    int right = dp[i][j+1];
-                    int down = dp[i+1][j];
-                    int diagonal = dp[i+1][j+1];
+        int next[] = new int[m + 1];
 
-                    int ans = 1+ Math.min(right,Math.min(down,diagonal));
-                    dp[i][j] = ans;
-                    max = Math.max(ans,max);
+        int max = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int curr[] = new int[m + 1];
+
+            for (int j = m - 1; j >= 0; j--) {
+                if (matrix[i][j] == '1') {
+                    int right = curr[j + 1];
+                    int down = next[j];
+                    int diagonal = next[j + 1];
+
+                    int ans = 1 + Math.min(right, Math.min(down, diagonal));
+                    curr[j] = ans;
+                    max = Math.max(ans, max);
                 }
             }
+            next = curr;
         }
-        return max*max;
+        return max * max;
     }
 }
