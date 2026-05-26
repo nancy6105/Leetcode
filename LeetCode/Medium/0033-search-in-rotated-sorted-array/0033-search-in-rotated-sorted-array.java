4f7288c51d3c36c9250974cc05589895e23ac19c
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-26 14:47:33 +0530

    0033-search-in-rotated-sorted-array 0 ms (100%) 43.7 MB (80.92%)

--- LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,46 +1,30 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int pivot = getPivot(nums);
-        if(pivot == 0){
-            return binarySearch(nums,0,nums.length-1,target);
-        }
-        if(target >= nums[0] && target <= nums[pivot-1]){
-            return binarySearch(nums,0,pivot-1,target);
-        }
-            return binarySearch(nums,pivot,nums.length-1,target);
-    }
-
-    int binarySearch(int arr[],int l,int r,int target){
-        while(l <= r){
-            int mid = l+(r-l)/2;
-            if(arr[mid] == target){
-                return mid;
-            }
-            if(arr[mid] < target){
-                l = mid+1;
-            }
-            else{
-                r = mid-1;
-            }
-        }
-        return -1;
-    }
-    int getPivot(int arr[]){
-        int n = arr.length;
+        int n = nums.length;
         int l = 0;
         int r = n-1;
 
-        while(l < r){
-            int mid = l + (r-l)/2;
+        while(l <= r){
+            int mid = l+(r-l)/2;
 
-            if(arr[mid] > arr[r]){
-                l = mid+1;
+            if(nums[mid] == target)return mid;
+            if(nums[l] <= nums[mid]){
+                if(target >= nums[l] && target < nums[mid]){
+                    r = mid-1;
+                }
+                else{
+                    l = mid+1;
+                }
             }
             else{
-
-                r = mid;
+                if(target > nums[mid] && target <= nums[r]){
+                    l = mid+1;
+                }
+                else{
+                    r = mid-1;
+                }
             }
         }
-        return l;
+        return -1;
     }
 }
