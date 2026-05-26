4efcb1e8b082e8db73f0773f39585799c86b4e53
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-26 17:40:03 +0530

    0034-find-first-and-last-position-of-element-in-sorted-array 0 ms (100%) 48.1 MB (69.38%)

--- LeetCode/Medium/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ LeetCode/Medium/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,22 +1,52 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int n = nums.length;
-        int first = -1;
-        int sec = -1;
+        int first = findFirst(nums,target);
+        int last = findLast(nums,target);
 
-        for(int i = 0;i<n;i++){
-            if(nums[i] == target){
-                first = i;
-                break;
+        return new int[]{first,last};
+    }
+
+    int findFirst(int nums[],int target){
+        int n = nums.length;
+        int l = 0;
+        int r = n-1;
+        int ans = -1;
+        while(l <= r){
+            int mid = l+(r-l)/2;
+            if(nums[mid] == target){
+                ans = mid;
+                r = mid-1;
+            }
+            else if(nums[mid] < target){
+                l = mid+1;
+            }
+            else{
+                r = mid-1;
             }
         }
+        return ans;
+    }
 
-        for(int i = n-1;i>=0;i--){
-            if(nums[i] == target){
-                sec = i;
-                break;
+    int findLast(int nums[], int target){
+        int n = nums.length;
+        int l = 0;
+        int r = n-1;
+
+        int ans = -1;
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if(nums[mid] == target){
+                ans = mid;
+                l = mid+1;
+            }
+            else if(nums[mid] < target){
+                l = mid+1;
+            }
+            else{
+                r = mid-1;
             }
         }
-        return new int[]{first,sec};
+        return ans;
     }
 }
