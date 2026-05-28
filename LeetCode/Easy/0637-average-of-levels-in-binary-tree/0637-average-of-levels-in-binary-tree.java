8dfcab75799aae3e32cf60a8d6df339e37ccfbbf
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-28 12:42:14 +0530

    0637-average-of-levels-in-binary-tree 2 ms (98.11%) 48.2 MB (87.49%)

--- LeetCode/Easy/0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
+++ LeetCode/Easy/0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
@@ -24,16 +24,12 @@ class Solution {
         while(!q.isEmpty()){
             int size = q.size();
             List<Integer> level = new ArrayList<>();
-            for(int i = 0;i<size;i++){
-                TreeNode node = q.poll();
-                level.add(node.val);
-                if(node.left != null) q.offer(node.left);
-                if(node.right != null) q.offer(node.right);
-            }
-
             double sum = 0;
             for(int i = 0;i<size;i++){
-                sum += level.get(i);
+                TreeNode node = q.poll();
+                sum += node.val;
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
             }
             res.add(sum/size);
         }
