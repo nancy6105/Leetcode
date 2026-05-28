9670cc57f66443f7f75f7d34326508a47c2543b7
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-28 12:33:16 +0530

    0199-binary-tree-right-side-view 1 ms (72.46%) 43.6 MB (66.75%)

--- LeetCode/Medium/0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ LeetCode/Medium/0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,19 +14,22 @@
  * }
  */
 class Solution {
-    List<Integer> arr;
     public List<Integer> rightSideView(TreeNode root) {
-        arr = new ArrayList<>();
-        dfs(root,0);
-        return arr;
-    }
-    void dfs(TreeNode root,int depth){
-        if(root == null)return;
-        if(depth == arr.size()){
-            arr.add(root.val);
+        List<Integer> res = new ArrayList<>();
+        if(root == null)return res;
+        Queue<TreeNode>q = new LinkedList<>();
+        q.offer(root);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0;i<size;i++){
+                TreeNode node = q.poll();
+                if(i == size-1)res.add(node.val);
+                if(node.left != null)q.offer(node.left);
+                if(node.right != null)q.offer(node.right);
+            }
         }
 
-        dfs(root.right,depth+1);
-        dfs(root.left,depth+1);
+        return res;
     }
 }
