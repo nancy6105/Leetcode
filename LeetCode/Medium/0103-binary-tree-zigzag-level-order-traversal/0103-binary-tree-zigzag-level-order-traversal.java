1988bd4ff06f3c20968aff56306b5efdf6cd3b06
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-28 13:58:02 +0530

    0103-binary-tree-zigzag-level-order-traversal 0 ms (100%) 43.8 MB (24.17%)

--- LeetCode/Medium/0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ LeetCode/Medium/0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -23,16 +23,19 @@ class Solution {
         boolean flag = true;
         while(!q.isEmpty()){
             int size = q.size();
-            List<Integer> level = new ArrayList<>();
-
+            LinkedList<Integer> level = new LinkedList<>();//dequeue;
             for(int i = 0;i<size;i++){
                 TreeNode node = q.poll();
-                level.add(node.val);
-                if(node.left!=null)q.offer(node.left);
-                if(node.right!=null)q.offer(node.right);
+                if(flag){
+                    level.addLast(node.val);
+                }
+                else{
+                    level.addFirst(node.val);
+                }
+                if(node.left != null)q.offer(node.left);
+                if(node.right != null)q.offer(node.right);
             }
-            if(!flag)Collections.reverse(level);
-            res.add(new ArrayList<>(level));
+            res.add(level);
             flag = !flag;
         }
         return res;
