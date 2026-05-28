da9c5e25a2fd60f7d94911ce6db56ea1a1a30534
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-05-28 14:55:50 +0530

    0101-symmetric-tree 0 ms (100%) 43.7 MB (15.34%)

---
+++ LeetCode/Easy/0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null)return true;
+
+        return sol(root.left,root.right);
+
+    }
+
+    boolean sol(TreeNode t1,TreeNode t2){
+        if(t1 == null && t2 == null)return true;
+        if(t1 == null || t2 == null)return false;
+
+        if(t1.val != t2.val)return false;
+
+        return sol(t1.left,t2.right) && sol(t1.right,t2.left);
+    }
+}
