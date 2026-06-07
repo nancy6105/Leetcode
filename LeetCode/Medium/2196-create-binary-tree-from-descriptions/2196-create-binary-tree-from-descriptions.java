c71392b32ee6419f01f99b85bf88c46e1a7acc60
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-06-07 23:16:41 +0530

    2196-create-binary-tree-from-descriptions 69 ms (43.13%) 86.3 MB (47.99%)

---
+++ LeetCode/Medium/2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        Map<Integer,TreeNode> mp = new HashMap<>();
+        Set<Integer> st = new HashSet<>();
+
+        for(int [] description : descriptions){
+            int parent = description[0];
+            int child = description[1];
+
+            boolean left = description[2] == 1;
+
+            mp.putIfAbsent(parent,new TreeNode(parent));
+            mp.putIfAbsent(child,new TreeNode(child));
+
+            if(left){
+                mp.get(parent).left = mp.get(child);
+            }
+            else{
+                mp.get(parent).right = mp.get(child);
+            }
+            st.add(child);
+        }
+
+         for (int[] description : descriptions) {
+            int parent = description[0];
+            if (!st.contains(parent)) {
+                return mp.get(parent);
+            }
+        }
+
+        return null;
+    }
+}
