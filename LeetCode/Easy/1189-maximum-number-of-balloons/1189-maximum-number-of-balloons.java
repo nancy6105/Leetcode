be6fe62dc5ddeaeeb0bd3dec8aa4aafbd1549e92
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-06-22 23:38:16 +0530

    1189-maximum-number-of-balloons 2 ms (96.67%) 43.5 MB (62.01%)

---
+++ LeetCode/Easy/1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int freq[] = new int[26];
+        for(char ch : text.toCharArray()){
+            freq[ch-'a']++;
+        }
+        int ans = Math.min(freq['b'-'a'],
+        Math.min(freq['a'-'a'],
+        Math.min(freq['l'-'a']/2,
+        Math.min(freq['o'-'a']/2,
+        freq['n'-'a']))));
+
+        return ans;
+    }
+}
