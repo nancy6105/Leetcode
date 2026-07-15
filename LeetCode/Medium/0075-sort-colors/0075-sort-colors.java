b0ad7dc1c35d26716c9e15c3461899cabf2d4fd6
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-07-15 08:35:55 +0530

    0075-sort-colors 0 ms (100%) 43.4 MB (77.5%)

---
+++ LeetCode/Medium/0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int i = 0;
+        int j = 0;
+        int k = n-1;
+
+        while(j<=k){
+            if(nums[j] == 1){
+                j++;
+            }
+            else if(nums[j] == 0){
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                i++;
+                j++;
+            }
+            else if(nums[j] == 2){
+                int temp = nums[k];
+                nums[k] = nums[j];
+                nums[j] = temp;
+                k--;
+            }
+        }
+    }
+}
