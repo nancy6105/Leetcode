6941497060346393be770420d34442e56ad32e3a
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-07-15 08:40:37 +0530

    0121-best-time-to-buy-and-sell-stock 1 ms (99.94%) 94.6 MB (33.1%)

--- LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,12 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int minimum_Price = prices[0];
         int profit = 0;
+        int min_price = prices[0];
+
 
         for(int price : prices){
-            profit = Math.max(profit,price-minimum_Price);
-            minimum_Price = Math.min(minimum_Price,price);
+            profit = Math.max(profit,price-min_price);
+            min_price = Math.min(min_price,price);
         }
+
         return profit;
     }
 }
