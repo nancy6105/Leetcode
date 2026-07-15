2c2e89596b4d094042197b4c5ecc11d39fdcf39c
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-07-15 12:14:32 +0530

    0031-next-permutation 0 ms (100%) 44.7 MB (51.74%)

--- LeetCode/Medium/0031-next-permutation/0031-next-permutation.java
+++ LeetCode/Medium/0031-next-permutation/0031-next-permutation.java
@@ -2,37 +2,41 @@ class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
         int idx = -1;
-        for(int i = n-1; i > 0; i--){
+        for(int i = n-1;i>0;i--){
             if(nums[i-1] < nums[i]){
                 idx = i-1;
                 break;
             }
         }
+
         if(idx != -1){
             int swapIdx = idx;
-            for(int j = n-1; j >= idx; j--){
+            for(int j = n-1;j>idx;j--){
                 if(nums[j] > nums[idx]){
-                    swapIdx = j;
+                   swapIdx = j;
                     break;
                 }
             }
-            swap(nums,swapIdx,idx);
+            swap(nums,idx,swapIdx);
         }
-        reverse(nums,idx+1);
+
+        reverse(nums,idx);
     }
 
-    void swap(int nums[],int l,int r){
-        int temp = nums[l];
-        nums[l] = nums[r];
-        nums[r] = temp;
+    void swap(int nums[],int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 
-    void reverse(int nums[],int j){
+    void reverse(int nums[],int idx){
         int n = nums.length;
-        while(j < n){
-            swap(nums,j,n-1);
-            j++;
-            n--;
+        int i = idx+1;
+        int j = n-1;
+        while(i < j){
+            swap(nums,i,j);
+            i++;
+            j--;
         }
     }
 }
