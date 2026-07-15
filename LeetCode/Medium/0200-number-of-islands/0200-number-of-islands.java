8de38648b113877543e16b06042dedeee9945db5
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-07-15 15:54:43 +0530

    0200-number-of-islands 3 ms (87.92%) 52.4 MB (33.8%)

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
@@ -6,25 +6,26 @@ class Solution {
         m = grid[0].length;
 
         int cnt = 0;
-        boolean visi[][] = new boolean[n][m];
+        //boolean visi[][] = new boolean[n][m];
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
                 if(grid[i][j] == '1'){
-                    dfs(i,j,grid,visi);
-                    cnt++;    
+                    dfs(i,j,grid);
+                    cnt++;
                 }
             }
         }
         return cnt;
     }
-    void dfs(int i,int j,char[][] grid,boolean visi[][]){
-        if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == '0'){
+
+    void dfs(int i,int j,char grid[][]){
+        if(i < 0 || i >= n || j < 0 || j >= m || grid[i][j] == '0'){
             return;
         }
         grid[i][j] = '0';
-        dfs(i+1,j,grid,visi);
-        dfs(i-1,j,grid,visi);
-        dfs(i,j+1,grid,visi);
-        dfs(i,j-1,grid,visi);
+        dfs(i+1,j,grid);
+        dfs(i-1,j,grid);
+        dfs(i,j+1,grid);
+        dfs(i,j-1,grid);
     }
 }
