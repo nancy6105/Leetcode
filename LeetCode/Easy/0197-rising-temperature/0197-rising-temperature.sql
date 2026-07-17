077900de4193272187901e6ad7a2b4df30540d8a
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-07-17 09:39:28 +0530

    0197-rising-temperature 400 ms (88.03%) 0B (100%)

---
+++ LeetCode/Easy/0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT w1.id 
+from weather w1
+join weather w2
+on w1.recordDate = Date_add(w2.recordDate,Interval 1 day)
+where w1.temperature > w2.temperature;
