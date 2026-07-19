f5d4a455f45205641a7dc35c9ff9071210b1c96d
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-07-19 22:51:59 +0530

    0300-longest-increasing-subsequence 413 ms (5.63%) 237.1 MB (16.02%)

--- LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,21 +1,25 @@
 class Solution {
+    int n;
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int next[] = new int[n+1];
-        
-        for(int idx = n-1; idx >= 0; idx--){
-            int curr[] = new int[n+1];
-            for(int prev = idx-1; prev >= -1; prev--){
-                int notpick = next[prev+1];
-                int pick = 0;
-                if(prev == -1 || nums[prev] < nums[idx]){
-                    pick = 1 + next[idx+1];
-                }
-                curr[prev+1] = Math.max(pick,notpick);
-            }
-            next = curr;
+        n = nums.length;
+        int dp[][] = new int[n][n+1];
+        for(int i = 0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return sol(0,-1,nums,dp);
+    }
+    int sol(int idx,int prev,int arr[],int dp[][]){
+        if(idx == n)return 0;
+
+        if(dp[idx][prev+1] != -1){
+            return dp[idx][prev+1];
         }
 
-        return next[0];
+        int notpick = 0 + sol(idx+1,prev,arr,dp);
+        int pick = 0;
+        if(prev == -1 || arr[idx] > arr[prev]){
+            pick = 1 + sol(idx+1,idx,arr,dp);
+        }
+        return dp[idx][prev+1] = Math.max(pick,notpick);
     }
 }
