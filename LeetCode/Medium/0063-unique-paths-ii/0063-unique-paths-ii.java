b8abfd99658c9b66e79387c9480e2957420491a1
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-07-19 12:04:32 +0530

    0063-unique-paths-ii 0 ms (100%) 43.6 MB (46.04%)

--- LeetCode/Medium/0063-unique-paths-ii/0063-unique-paths-ii.java
+++ LeetCode/Medium/0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,48 +1,34 @@
 class Solution {
+    int n;
+    int m;
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int n = obstacleGrid.length;
-        int m = obstacleGrid[0].length;
-
+        n = obstacleGrid.length;
+        m = obstacleGrid[0].length;
         int dp[][] = new int[n][m];
-        for(int i = n-1; i >= 0; i--){
-            for(int j = m-1;j >= 0; j--){
-                if(obstacleGrid[i][j] == 1){
-                    dp[i][j] = 0;
-                }
-                else if(i == n-1 && j == m-1){
-                    dp[i][j] = 1;
-                }
-
-                else{
-                    int down = 0;
-                    int right = 0;
-                    if(i < n-1){
-                        down = dp[i+1][j];
-                    }
-                    if(j < m-1){
-                        right = dp[i][j+1]; 
-                    }
-                    dp[i][j] = down+right;
-                }
-            }
+        for(int i = 0;i<n;i++){
+            Arrays.fill(dp[i],-1);
         }
-        return dp[0][0];
+        return sol(n-1,m-1,obstacleGrid,dp);
     }
     int sol(int i,int j,int[][] arr,int dp[][]){
-        int n = arr.length;
-        int m = arr[0].length;
-        if(i < n && j < m && arr[i][j] == 1){
-            return 0;   
+        if(i >= 0 && j >= 0 && arr[i][j] == 1){
+            return 0;
         }
-        if(i == n-1 && j == m-1){
+        
+        if(i < 0 || j < 0){
+            return 0;
+        }
+
+        if(i == 0 && j == 0){
             return 1;
         }
-        if(i >= n || j >= m)return 0;
+        
 
-        if(dp[i][j] != -1)return dp[i][j];
-
-        int down = sol(i+1,j,arr,dp);
-        int right = sol(i,j+1,arr,dp);
-        return dp[i][j] = down + right;
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int up = sol(i-1,j,arr,dp);
+        int left = sol(i,j-1,arr,dp);
+        return dp[i][j] = up+left;
     }
 }
