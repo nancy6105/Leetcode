ff51f5aa0efb11ad2bf7a0a4825e484c011b8ff7
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-07-19 11:23:40 +0530

    0064-minimum-path-sum 7 ms (10.54%) 50.4 MB (38.36%)

--- LeetCode/Medium/0064-minimum-path-sum/0064-minimum-path-sum.java
+++ LeetCode/Medium/0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -5,25 +5,26 @@ class Solution {
         n = grid.length;
         m = grid[0].length;
         int dp[][] = new int[n][m];
-        for(int i = 0;i<n;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return sol(0,0,grid,dp);
-    }
-    int sol(int i,int j,int arr[][],int dp[][]){
-        if(i >= n || j >= m){
-            return (int)1e9;
-        }
-        if(i == n-1 && j == m-1){
-            return arr[i][j];
-        }
+        for(int i = 0; i< n; i++){
+            for(int j = 0; j < m; j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = grid[i][j];
+                }
+                else{
+                    int left = Integer.MAX_VALUE;
+                    int up = Integer.MAX_VALUE;
 
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
+                    if(i > 0){
+                        up = grid[i][j] + dp[i-1][j];
+                    }
+                    if(j > 0){
+                        left = grid[i][j] + dp[i][j-1];
+                    }
 
-        int down = arr[i][j] + sol(i+1,j,arr,dp);
-        int right = arr[i][j] + sol(i,j+1,arr,dp);
-        return dp[i][j] = Math.min(down,right);
+                    dp[i][j] = Math.min(up,left);
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 }
