d3f886b942f375e8873f2d4b26d7184e220be52c
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-07-19 15:06:47 +0530

    0072-edit-distance 3 ms (98.36%) 47.2 MB (53.54%)

--- LeetCode/Medium/0072-edit-distance/0072-edit-distance.java
+++ LeetCode/Medium/0072-edit-distance/0072-edit-distance.java
@@ -1,30 +1,32 @@
 class Solution {
-    public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
+    int n;
+    int m;
 
-        int dp[][] = new int[n][m];
-        for(int row[] : dp){
-            Arrays.fill(row,-1);
+    public int minDistance(String word1, String word2) {
+        n = word1.length();
+        m = word2.length();
+        int dp[][] = new int[n+1][m+1];
+        for(int i = 0;i<=n;i++){
+            Arrays.fill(dp[i],-1);
         }
-        return sol(0,0,word1,word2,dp);
+
+        return sol(0,0,word1,word2,dp);    
     }
+
     int sol(int i, int j, String s1, String s2,int dp[][]){
-        if(i == s1.length())return s2.length()-j;
-        if(j == s2.length())return s1.length()-i;
+        if(i == n)return dp[n][j] = m-j;
+        if(j == m)return dp[i][m] = n-i;
 
         if(dp[i][j] != -1){
             return dp[i][j];
         }
         if(s1.charAt(i) == s2.charAt(j)){
-            return sol(i+1,j+1,s1,s2,dp);
+            return dp[i][j] = sol(i+1,j+1,s1,s2,dp);
         }
+        int replace = sol(i+1,j+1,s1,s2,dp);
+        int delete = sol(i+1,j,s1,s2,dp);
+        int insert = sol(i,j+1,s1,s2,dp);
 
-        int replace = sol(i + 1, j + 1,s1, s2,dp);
-        int delete = sol(i + 1, j,s1, s2,dp);
-        int insert = sol(i, j + 1,s1, s2,dp);
-
-        return dp[i][j] = 1 + Math.min(replace,Math.min(delete,insert));
-
+        return dp[i][j] = 1 + Math.min(replace,Math.min(insert,delete));
     }
 }
