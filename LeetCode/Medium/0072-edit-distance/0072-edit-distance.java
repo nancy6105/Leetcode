7381e8b2f92a3da76a8f2af84c65a514e4b180ad
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-07-19 15:36:31 +0530

    0072-edit-distance 5 ms (68.14%) 47.1 MB (53.54%)

--- LeetCode/Medium/0072-edit-distance/0072-edit-distance.java
+++ LeetCode/Medium/0072-edit-distance/0072-edit-distance.java
@@ -7,10 +7,28 @@ class Solution {
         m = word2.length();
         int dp[][] = new int[n+1][m+1];
         for(int i = 0;i<=n;i++){
-            Arrays.fill(dp[i],-1);
+            dp[i][m] = n-i;
         }
 
-        return sol(0,0,word1,word2,dp);    
+        for(int j = 0;j<=m;j++){
+            dp[n][j] = m-j;
+        }
+
+        for(int i = n-1; i >= 0; i--){
+            for(int j = m-1; j >= 0; j--){
+                if(word1.charAt(i) == word2.charAt(j)){
+                    dp[i][j] = dp[i+1][j+1];
+                }
+                else{
+                    int replace = dp[i+1][j+1];
+                    int del = dp[i+1][j];
+                    int inst = dp[i][j+1];
+
+                    dp[i][j] = 1 + Math.min(replace,Math.min(inst,del));
+                }
+            }
+        }
+        return dp[0][0];    
     }
 
     int sol(int i, int j, String s1, String s2,int dp[][]){
