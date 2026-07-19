6c8dd6876b0aa46d5348955b1a6a47878f946ade
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-07-19 10:53:47 +0530

    0064-minimum-path-sum 1 ms (99.92%) 50.2 MB (72.4%)

--- LeetCode/Medium/0064-minimum-path-sum/0064-minimum-path-sum.java
+++ LeetCode/Medium/0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,41 +1,21 @@
 class Solution {
+    int n;
+    int m;
     public int minPathSum(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        
+        n = grid.length;
+        m = grid[0].length;
         int dp[][] = new int[n][m];
-
-        for(int i = n-1; i >= 0; i--){
-            for(int j = m-1; j >= 0; j--){
-
-                if(i == n-1 && j == m-1){
-                    dp[i][j] = grid[i][j];
-                }
-
-                else{
-                    int down = (int)1e9;
-                    int right = (int)1e9;
-
-                    if(i+1 < n){
-                        down = grid[i][j] + dp[i+1][j];
-                    }
-                    if(j+1 < m){
-                        right = grid[i][j] + dp[i][j+1];
-                    }
-
-                    dp[i][j] = Math.min(right,down);
-                }
-            }
+        for(int i = 0;i<n;i++){
+            Arrays.fill(dp[i],-1);
         }
-        return dp[0][0];
+        return sol(0,0,grid,dp);
     }
-    public int sol(int i, int j, int[][]arr, int dp[][]){
-        if(i == arr.length-1 && j == arr[0].length-1){
-            return arr[i][j];
-        }
-
-        if(i >= arr.length || j >= arr[0].length){
+    int sol(int i,int j,int arr[][],int dp[][]){
+        if(i >= n || j >= m){
             return (int)1e9;
+        }
+        if(i == n-1 && j == m-1){
+            return arr[i][j];
         }
 
         if(dp[i][j] != -1){
@@ -43,8 +23,7 @@ class Solution {
         }
 
         int down = arr[i][j] + sol(i+1,j,arr,dp);
-        int diagonal = arr[i][j] + sol(i,j+1,arr,dp);
-
-        return dp[i][j] = Math.min(down,diagonal);
+        int right = arr[i][j] + sol(i,j+1,arr,dp);
+        return dp[i][j] = Math.min(down,right);
     }
 }
