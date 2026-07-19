1643d9933f3ef43fe0cbb64df8c98205b81c87ea
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-07-19 17:02:33 +0530

    0188-best-time-to-buy-and-sell-stock-iv 11 ms (9.11%) 48.2 MB (7.34%)

--- LeetCode/Hard/0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ LeetCode/Hard/0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,27 +1,38 @@
 class Solution {
+    int n;
     public int maxProfit(int k, int[] prices) {
-        int n = prices.length;
-        int after[][] = new int[2][k+1];
-        for(int idx = n-1;idx >= 0;idx--){
-            int curr[][] = new int[2][k+1];
-            for(int buy = 0;buy <= 1;buy++){
-                for(int trans = 1; trans < k+1; trans++){
-                    int profit = 0;
-                    if(buy == 1){
-                        int take = -prices[idx] + after[0][trans];
-                        int nottake = 0 + after[1][trans];
-                        profit = Math.max(take,nottake);
-                    }
-                    else{
-                        int take = prices[idx] + after[1][trans-1];
-                        int nottake = 0 + after[0][trans];
-                        profit = Math.max(take,nottake);
-                    }
-                    curr[buy][trans] = profit;
-                }
+        n = prices.length;
+        int dp[][][] = new int[n+1][k+1][2];
+        for(int i = 0;i<=n;i++){
+            for(int j = 0;j<=k;j++){
+                Arrays.fill(dp[i][j],-1);
             }
-            after = curr;
         }
-        return after[1][k];
+        return sol(0,k,1,prices,dp);
     }
+    int sol(int idx,int k,int buy,int [] arr,int dp[][][]){
+        if(k == 0){
+            return dp[idx][k][buy] = 0;
+        }
+        if(idx == n){
+            return dp[idx][k][buy] = 0;
+        }
+
+        if(dp[idx][k][buy] != -1){
+            return dp[idx][k][buy];
+        }
+        int profit = 0;
+        if(buy == 1){
+            int take = -arr[idx]+sol(idx+1,k,0,arr,dp);
+            int nottake = 0 + sol(idx+1,k,1,arr,dp);
+            profit = Math.max(take,nottake);
+        }
+        else{
+            int take = arr[idx] + sol(idx+1,k-1,1,arr,dp);
+            int nottake = 0 + sol(idx+1,k,0,arr,dp);
+            profit = Math.max(take,nottake);
+        }
+
+        return dp[idx][k][buy] = profit;
+    }      
 }
