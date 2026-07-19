df8e075fd858cc65ca4de3ce8dad47e91c9a1c0b
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-07-19 21:58:34 +0530

    0139-word-break 7 ms (60.26%) 46.1 MB (49.6%)

--- LeetCode/Medium/0139-word-break/0139-word-break.java
+++ LeetCode/Medium/0139-word-break/0139-word-break.java
@@ -1,20 +1,32 @@
 class Solution {
+    int n;
     public boolean wordBreak(String s, List<String> wordDict) {
         HashSet<String> st = new HashSet<>(wordDict);
-        int n = s.length();
-        int dp[] = new int[n+1];
-        dp[n] = 1;
-        for(int i = n-1; i >= 0; i--){
-            for(int j = i; j < n; j++){
-                String sub = s.substring(i,j+1); 
-                if(st.contains(sub)){
-                    if(dp[j+1] == 1){
-                        dp[i] = 1;
-                        break;
-                    }
+        n = s.length();
+        int dp[] = new int[n];
+        Arrays.fill(dp,-1);
+        return sol(0,s,st,dp);
+    }
+    boolean sol(int idx,String s,HashSet<String>st,int dp[]){
+
+        if(idx == n){
+            return true;
+        }
+
+        if(dp[idx] != -1){
+            return dp[idx] == 1;
+        }
+        for(int i = idx;i < n;i++){
+            String sub = s.substring(idx,i+1);
+            if(st.contains(sub)){
+                if(sol(i+1,s,st,dp)){
+                    dp[idx] = 1;
+                    return true;
                 }
             }
         }
-        return dp[0] == 1;
+        dp[idx] = 0;
+        return false;
     }
+    
 }
