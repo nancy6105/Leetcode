758f83b9e2df6a3bbb3652e7a0c3447a2c488416
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-07-19 10:17:41 +0530

    0120-triangle 4 ms (43.82%) 45.6 MB (50.27%)

--- LeetCode/Medium/0120-triangle/0120-triangle.java
+++ LeetCode/Medium/0120-triangle/0120-triangle.java
@@ -1,24 +1,31 @@
 class Solution {
+    int n;
+    int m;
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        int nextRow[] = new int[n];
-        for(int j = 0; j < n; j++){
-            nextRow[j] = triangle.get(n-1).get(j);
+        n = triangle.size();
+        int dp[][] = new int[n][n];
+        for(int j = 0;j<n;j++){
+            dp[n-1][j] = triangle.get(n-1).get(j);
         }
 
-        for(int i = n-2; i >= 0; i--){
-            int currRow[] = new int[n];
-
-            for(int j = 0; j <= i; j++){
-
-                int down = triangle.get(i).get(j) + nextRow[j];
-                int diagonal = triangle.get(i).get(j) + nextRow[j+1];
-
-                currRow[j] = Math.min(down,diagonal);
+        for(int i = n-2;i>=0;i--){
+            for(int j = 0;j<=i;j++){
+                dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i+1][j],dp[i+1][j+1]);
             }
-            nextRow = currRow;
+        }
+        return dp[0][0];
+    }
+    int sol(int i, int j, List<List<Integer>> arr,int dp[][]){
+        if(i == n-1){
+            return arr.get(i).get(j);
         }
 
-        return nextRow[0];
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int down = arr.get(i).get(j) + sol(i+1,j,arr,dp);
+        int diag = arr.get(i).get(j) + sol(i+1,j+1,arr,dp);
+
+        return dp[i][j] = Math.min(down,diag); 
     }
 }
