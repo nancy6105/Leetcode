96842c4815af661831a2cd1ce20c5cf5d6438f2f
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-07-19 15:45:02 +0530

    0072-edit-distance 4 ms (76.84%) 46.5 MB (94.21%)

--- LeetCode/Medium/0072-edit-distance/0072-edit-distance.java
+++ LeetCode/Medium/0072-edit-distance/0072-edit-distance.java
@@ -5,46 +5,28 @@ class Solution {
     public int minDistance(String word1, String word2) {
         n = word1.length();
         m = word2.length();
-        int dp[][] = new int[n+1][m+1];
-        for(int i = 0;i<=n;i++){
-            dp[i][m] = n-i;
-        }
-
+        int next[] = new int[m+1];
         for(int j = 0;j<=m;j++){
-            dp[n][j] = m-j;
+            next[j] = m-j;
         }
 
         for(int i = n-1; i >= 0; i--){
+            int curr[] = new int[m+1];
+            curr[m] = n-i;
             for(int j = m-1; j >= 0; j--){
                 if(word1.charAt(i) == word2.charAt(j)){
-                    dp[i][j] = dp[i+1][j+1];
+                    curr[j] = next[j+1];
                 }
                 else{
-                    int replace = dp[i+1][j+1];
-                    int del = dp[i+1][j];
-                    int inst = dp[i][j+1];
+                    int replace = next[j+1];
+                    int del = next[j];
+                    int inst = curr[j+1];
 
-                    dp[i][j] = 1 + Math.min(replace,Math.min(inst,del));
+                    curr[j] = 1 + Math.min(replace,Math.min(inst,del));
                 }
             }
+            next = curr;
         }
-        return dp[0][0];    
-    }
-
-    int sol(int i, int j, String s1, String s2,int dp[][]){
-        if(i == n)return dp[n][j] = m-j;
-        if(j == m)return dp[i][m] = n-i;
-
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        if(s1.charAt(i) == s2.charAt(j)){
-            return dp[i][j] = sol(i+1,j+1,s1,s2,dp);
-        }
-        int replace = sol(i+1,j+1,s1,s2,dp);
-        int delete = sol(i+1,j,s1,s2,dp);
-        int insert = sol(i,j+1,s1,s2,dp);
-
-        return dp[i][j] = 1 + Math.min(replace,Math.min(insert,delete));
+        return next[0];    
     }
 }
