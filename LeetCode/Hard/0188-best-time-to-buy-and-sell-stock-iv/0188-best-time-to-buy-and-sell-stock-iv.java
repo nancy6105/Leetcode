2a023bb9227eb403a88e4d7256b15f20003df42a
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-07-19 18:10:34 +0530

    0188-best-time-to-buy-and-sell-stock-iv 8 ms (28.7%) 46.5 MB (15.41%)

--- LeetCode/Hard/0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ LeetCode/Hard/0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -3,36 +3,27 @@ class Solution {
 
     public int maxProfit(int k, int[] prices) {
         n = prices.length;
-        int dp[][][] = new int[n + 1][k + 1][2];
-        for (int idx = 0; idx <= n; idx++) {
-            for (int buy = 0; buy < 2; buy++) {
-                dp[idx][0][buy] = 0;
-            }
-        }
-
-        for (int j = 0; j <= k; j++) {
-            for (int buy = 0; buy < 2; buy++) {
-                dp[n][j][buy] = 0;
-            }
-        }
+        int after[][] = new int[k+1][2];
 
         for (int idx = n - 1; idx >= 0; idx--) {
+            int curr[][] = new int[k+1][2];
             for (int j = 1; j <= k; j++) {
                 for (int buy = 0; buy < 2; buy++) {
                     int profit = 0;
                     if (buy == 1) {
-                        int take = -prices[idx] + dp[idx + 1][j][0];
-                        int nottake = 0 + dp[idx + 1][j][1];
+                        int take = -prices[idx] + after[j][0];
+                        int nottake = 0 + after[j][1];
                         profit = Math.max(take, nottake);
                     } else {
-                        int take = prices[idx] + dp[idx + 1][j - 1][1];
-                        int nottake = 0 + dp[idx + 1][j][0];
+                        int take = prices[idx] + after[j - 1][1];
+                        int nottake = 0 + after[j][0];
                         profit = Math.max(take, nottake);
                     }
-                    dp[idx][j][buy] = profit;
+                    curr[j][buy] = profit;
                 }
             }
+            after = curr;
         }
-        return dp[0][k][1];
+        return after[k][1];
     }
 }
