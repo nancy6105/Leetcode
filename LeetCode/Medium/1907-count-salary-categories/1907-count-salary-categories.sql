4544c68c0a783ac75073d9b3fa70cdbb2eab995d
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-07-20 23:16:39 +0530

    1907-count-salary-categories 1766 ms (16.27%) 0B (100%)

--- LeetCode/Medium/1907-count-salary-categories/1907-count-salary-categories.sql
+++ LeetCode/Medium/1907-count-salary-categories/1907-count-salary-categories.sql
@@ -5,12 +5,12 @@ where income < 20000
 
 union all
 
-select "Average Salary" as category, count(*)
+select "Average Salary" as category, count(*) as accounts_count
 from accounts
 where income between 20000 and 50000
 
 union all 
 
-select "High Salary" as category, count(*)
+select "High Salary" as category, count(*) as accounts_count
 from accounts
 where income > 50000
