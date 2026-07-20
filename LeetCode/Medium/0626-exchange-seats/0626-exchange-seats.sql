16da78b846996c7d2b0e3993ff06f15055a9af17
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-07-20 23:28:07 +0530

    0626-exchange-seats 380 ms (32.04%) 0B (100%)

---
+++ LeetCode/Medium/0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT id,
+CASE WHEN id%2 = 0 THEN
+LAG(student,1) OVER (ORDER BY id)
+WHEN id%2 = 1 THEN
+IFNULL(LEAD(student,1) OVER (ORDER BY id),student)
+END AS student
+FROM Seat
