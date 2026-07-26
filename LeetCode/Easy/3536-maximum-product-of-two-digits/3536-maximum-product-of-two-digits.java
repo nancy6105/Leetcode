135c9e38457c5b1ec2541619ab9c1ef459edc65d
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-07-27 00:20:02 +0530

    3536-maximum-product-of-two-digits 1 ms (100%) 42.5 MB (74.31%)

--- LeetCode/Easy/3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ LeetCode/Easy/3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -1,13 +1,18 @@
 class Solution {
     public int maxProduct(int n) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        while(n > 0){
-            arr.add(n%10);
-            n = n/10;
-        }
+        int max1 = 0;
+        int max2 = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            if (digit > max1) {
 
-        Collections.sort(arr);
-        int m = arr.size();
-        return arr.get(m-1) * arr.get(m-2);
+                max2 = max1;
+                max1 = digit;
+            } else if (digit > max2) {
+                max2 = digit;
+            }
+            n /= 10;
+        }
+        return max1 * max2;
     }
 }
