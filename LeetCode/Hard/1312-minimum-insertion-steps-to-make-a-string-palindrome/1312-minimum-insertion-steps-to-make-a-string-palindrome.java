959953f7ac973e75f59d7bf33004dea459a51b2d
Nancy Lodhi <142438655+nancy6105@users.noreply.github.com>
2026-07-27 13:13:34 +0530

    1312-minimum-insertion-steps-to-make-a-string-palindrome 21 ms (75.76%) 46.7 MB (78.3%)

---
+++ LeetCode/Hard/1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,33 @@
+class Solution {
+    int n;
+    public int minInsertions(String s) {
+        n = s.length();
+        int len = longestPalindromeSubseq(s);
+        return n - len;
+    }
+    public int longestPalindromeSubseq(String s) {
+        n = s.length();
+        int dp[][] = new int[n][n];
+        for(int row[] : dp){
+            Arrays.fill(row,-1);
+        }
+        return sol(0,n-1,s,dp);
+    }
+    int sol(int i,int j,String s,int dp[][]){
+        if(i > j){
+            return 0;
+        }
+        if(i == j){
+            return 1;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        if(s.charAt(i) == s.charAt(j)){
+            return dp[i][j] = 2 + sol(i+1,j-1,s,dp);
+        }
+
+        return dp[i][j] = Math.max(sol(i+1,j,s,dp),sol(i,j-1,s,dp));
+    }
+}
